a82a39564e538402ae188397db726dca552ca7c7
John Francis <john9francis@gmail.com>
2024-01-31 12:11:36 -0700

    got the pendulum to match our initial conditions that we had for the rest of the program

--- pendulum_demo.py
+++ pendulum_demo.py
@@ -1,7 +1,6 @@
 # This file is an animated demonstration of a driven-damped pendulum
 
 import pygame
-import sys
 import math
 
 # my own classes
@@ -16,6 +15,10 @@ class Theta:
     def update(self, dt):
         # always move left
         self.pos.x -= self.speed * dt
+
+        # delete self if leaves screen
+        if self.pos.x < 0:
+            thetas.remove(self)
         pass
     
     def draw(self, screen):
@@ -27,8 +30,7 @@ pygame.init()
 
 # Constants
 width, height = 1200, 800
-fps = 60
-dt = 1 / fps
+dt = .007
 
 # Colors
 black = (0, 0, 0)
@@ -37,14 +39,14 @@ red = (255, 0, 0)
 
 
 # Pendulum parameters
-length = 150
-theta = math.pi / 4
+length = 9.8
+theta = .2
 omega = 0
 alpha = 0
-g = 9.81 * 100
-damping = 1
-amplitude = 16
-frequency = 2
+g = 9.8
+damping = .5
+amplitude = 1.2
+frequency = 2/3
 
 # make a list of thetas
 thetas = []
@@ -57,13 +59,15 @@ pygame.display.set_caption("Driven-Damped Pendulum Simulation")
 clock = pygame.time.Clock()
 running = True
 
+time = 0
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     # Update pendulum motion equations
-    alpha = -g / length * math.sin(theta) - damping * omega + amplitude * math.sin(frequency * pygame.time.get_ticks() / 1000)
+    alpha = -g / length * math.sin(theta) - damping * omega + amplitude * math.sin(frequency * time)
     omega += alpha * dt
     theta += omega * dt
 
@@ -72,13 +76,17 @@ while running:
 
     # Draw pendulum
     y_shift = 150
-    pendulum_x = width // 2 + length * math.sin(theta)
-    pendulum_y = height // 2 + length * math.cos(theta) + y_shift
+    scale_factor = 20
+
+    scaled_length = scale_factor * length
+
+    pendulum_x = width // 2 + scaled_length * math.sin(theta)
+    pendulum_y = height // 2 + scaled_length * math.cos(theta) + y_shift
     pygame.draw.line(screen, white, (width // 2, height // 2 + y_shift), (pendulum_x, pendulum_y), 5)
     pygame.draw.circle(screen, white, (int(pendulum_x), int(pendulum_y)), 15)
 
     # draw theta graph
-    theta_pos = pygame.Vector2(width // 2, height // 2 - length * math.cos(theta) - 200)
+    theta_pos = pygame.Vector2(width // 2, height // 2 - scaled_length/2 * math.cos(theta) - 200)
     th = Theta(theta_pos)
     thetas.append(th)
 
@@ -89,8 +97,8 @@ while running:
     # Update display
     pygame.display.flip()
 
-    # Cap the frame rate
-    clock.tick(fps)
+    # increment time
+    time += dt
 
 # Quit pygame
 pygame.quit()
